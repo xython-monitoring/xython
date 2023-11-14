xython-monitoring/xython

commit 31dd0befe7275b04d58c30c63f0cd61078832d46

Handle RRDWIDTH/RRDHEIGHT
Signed-off-by: LABBE Corentin <clabbe.montjoie@gmail.com>

xython/xython.py
@@ -225,6 +225,8 @@ class xythonsrv:
         self.rrd_column["sensor"] = ['sensor']
         # timings
         self.RRD_INTERVAL = 5 * 60
+        self.RRDWIDTH = 576
+        self.RRDHEIGHT = 140
         # at which interval state/client send their status
         # default 5 minute
         self.ST_INTERVAL = 5 * 60
@@ -1869,8 +1871,16 @@ class xythonsrv:
                 os.mkdir(basedir)
                 os.chmod(basedir, 0o755)
             pngpath = f"{self.wwwdir}/{hostname}/{graph}.png"
+            if 'RRDWIDTH' in self.xymonserver_cfg:
+                width = self.xymonserver_cfg['RRDWIDTH']
+            else:
+                width = self.RRDWIDTH
+            if 'RRDHEIGHT' in self.xymonserver_cfg:
+                height = self.xymonserver_cfg['RRDHEIGHT']
+            else:
+                width = self.RRDHEIGHT
             base = [pngpath,
-                '--width=576', '--height=140',
+                f'--width={width}', f'--height={height}',
                 '--vertical-label="% Full"',
                 '--start=end-96h'
                 ]
@@ -1979,10 +1989,18 @@ class xythonsrv:
         self.debug(rrdlist)
         if len(rrdlist) == 0:
             return 'Status: 400 Bad Request\n\nERROR: RRD list is empty'
+        if 'RRDWIDTH' in self.xymonserver_cfg:
+            width = self.xymonserver_cfg['RRDWIDTH']
+        else:
+            width = self.RRDWIDTH
+        if 'RRDHEIGHT' in self.xymonserver_cfg:
+            height = self.xymonserver_cfg['RRDHEIGHT']
+        else:
+            height = self.RRDHEIGHT
         base = ['-',
-        '--width=576', '--height=140',
-        '--vertical-label="% Full"',
-        '--start=end-96h'
+                f'--width={width}', f'--height={height}',
+            '--vertical-label="% Full"',
+            '--start=end-96h'
         ]
         if 'RRDGRAPHOPTS' in self.xymonserver_cfg:
             for rrdgopt in self.xymonserver_cfg['RRDGRAPHOPTS'].split(' '):

commit 42573b45d73f00e32ed6b3d6187cd615abe7f313

rrd: Fix a typo
In RRDWIDTH/RRDHEIGHT, I did and error when setting heigth.

Fixes: 31dd0bef ("Handle RRDWIDTH/RRDHEIGHT")
Signed-off-by: LABBE Corentin <clabbe.montjoie@gmail.com>

xython/xython.py
@@ -2037,7 +2037,7 @@ class xythonsrv:
             if 'RRDHEIGHT' in self.xymonserver_cfg:
                 height = self.xymonserver_cfg['RRDHEIGHT']
             else:
-                width = self.RRDHEIGHT
+                height = self.RRDHEIGHT
             base = [pngpath,
                 f'--width={width}', f'--height={height}',
                 '--vertical-label="% Full"',