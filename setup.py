6b48094778f9f3a6252f1686c8b1d4756f7261cd
LABBE Corentin <clabbe.montjoie@gmail.com>
2025-08-13 11:58:34 +0200

    Drop python 3.8 as it is End of life
    python 3.8 is end of life, and pysnmp tests fail on python3.8 only
    showing probably some bug.
    Since the older supported OS is bullseye with python3.9, lets drop 3.8.
    
    Signed-off-by: LABBE Corentin <clabbe.montjoie@gmail.com>

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from codecs import open
 from setuptools import setup
 
 CURRENT_PYTHON = sys.version_info[:2]
-REQUIRED_PYTHON = (3, 6)
+REQUIRED_PYTHON = (3, 9)
 
 if CURRENT_PYTHON < REQUIRED_PYTHON:
     sys.stderr.write(
