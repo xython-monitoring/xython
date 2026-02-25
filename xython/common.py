659e452661031af2b0ea0af08e1f450538795aca
LABBE Corentin <clabbe.montjoie@gmail.com>
2026-02-25 22:40:57 +0100

    Prevent empty hostname
    Signed-off-by: LABBE Corentin <clabbe.montjoie@gmail.com>

--- xython/common.py
+++ xython/common.py
@@ -219,12 +219,12 @@ def xydelay(s):
 
 
 def is_valid_hostname(hostname):
-    if hostname is None:
+    if not hostname:
         return False
     # should not start or end with .
     if hostname[0] == '.' or hostname[-1] == '.':
         return False
-    return re.match("^[a-zA-Z0-9_.-]*$", hostname)
+    return re.match("^[a-zA-Z0-9_.-]+$", hostname)
 
 
 def is_valid_column(column):
